c982cd6e509d18c0a35096cc5a0fa8a86ede182a
Hackbright Student <info@hackbrightacademy.com>
2015-10-01 16:44:06 -0700

    read_files doesn't crash

--- markov.py
+++ markov.py
@@ -4,18 +4,34 @@ from random import choice
 
 class SimpleMarkovGenerator(object):
 
-    def read_files(self, filenames):
+    def __init__(self, filepaths):
+        self.input_filepaths = filepaths
+        self.chains = {}
+
+
+    def read_files(self):
         """Given a list of files, make chains from them."""
 
-        # your code here
+
+        for filepath in self.input_filepaths:
+            text_file = open(filepath)
+            full_text_string = text_file.read()
+            text_file.close()
+
+            # add chains from the current file to the overall chains dictionary
+            self.make_chains(full_text_string)
+
+        print self.chains
 
 
-    def make_chains(self, corpus):
+    def make_chains(self, input_text_string):
         """Takes input text as string; stores chains."""
 
-        # your code here
+        # your code here which works on self.chains
 
-    def make_text(self):
+        #don't return chains
+
+    def make_text(self, chains):
         """Takes dictionary of markov chains; returns random text."""
 
         # your code here
